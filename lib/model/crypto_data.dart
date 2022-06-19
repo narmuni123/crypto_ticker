13853379d0f09156fc1b035f0fc0bdb7fde01bcc
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-19 21:48:16 +0530

    Added internet permission

--- lib/model/crypto_data.dart
+++ lib/model/crypto_data.dart
@@ -29,6 +29,7 @@ const List<String> currenciesList = [
 const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+
 const apiKey = 'YOUR-API-KEY-HERE';
 
 class CoinData {
